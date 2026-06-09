846b60569b65aa301dc4033564420344e7273ba8
SteveTheKiller <steve@thekiller.net>
2026-06-09 09:28:14 -0700

    WURSA fix

--- WURSA.ps1
+++ WURSA.ps1
@@ -171,6 +171,7 @@ try {
     $ServiceManager = New-Object -ComObject Microsoft.Update.ServiceManager
     $ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d", 7, "") | Out-Null
     Write-StepUpdate -Success
+    Start-Sleep -Seconds 3
 } catch {
     Write-Host "      [!] Failed: $($_.Exception.Message)" -ForegroundColor Red
 }
